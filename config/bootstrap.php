ac6cdbf1daa8e745c403032810b866769b4b7bae
Nam Nguyen <nam.ngvan@gmail.com>
2016-11-20 15:55:58 +0700

    no message

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,12 +4,8 @@ include __DIR__ . '/constants.global.php';
 
 $autoloader = include __DIR__ . '/../vendor/autoload.php';
 
-services()->set('autoloader', $autoloader)->set('eventManager', events())
+services()
+    ->set('autoloader', $autoloader)->set('eventManager', events())
     ->set('configManager', configs());
 
 configs()->loadModularConfig();
-
-$autoloader->setPsr4('Auth\\', [
-    __DIR__ . '/../package/phpfox-module-auth/src',
-    __DIR__ . '/../package/phpfox-module-auth/test',
-]);
